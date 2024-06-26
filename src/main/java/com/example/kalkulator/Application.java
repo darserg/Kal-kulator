2b42350013e8b5c4299ca301b2b5b8886b4d804b
darserg <63180230+darserg@users.noreply.github.com>
2024-06-26 18:14:39 +0400

    Deleted uselles function

--- src/main/java/com/example/kalkulator/Application.java
+++ src/main/java/com/example/kalkulator/Application.java
@@ -9,16 +9,12 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        String stylesheet = getClass().getResource("/style/style.css").toExternalForm();
+        String stylesheet = getClass().getResource("style/style.css").toExternalForm();
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 300, 50);
         scene.getStylesheets().add(stylesheet); // вместо stylesheet пробовал прописывать путь к файлу, он его не видит
         stage.setTitle("ThisIsCalc!");
         stage.setScene(scene);
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
     }
 }
--- src/main/java/com/example/kalkulator/Launcher.java
+++ src/main/java/com/example/kalkulator/Launcher.java
@@ -2,6 +2,6 @@ package com.example.kalkulator;
 
 public class Launcher {
     public static void main(String[] args) {
-        Application.main(args);
+        Application.start();
     }
 }
